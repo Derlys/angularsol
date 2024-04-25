79a21c981eb2e7ceb5e8340532301058e6982d5b
Derlys Dominguez <derlys275@gmail.com>
2024-04-25 16:00:12 +0200

    implement routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import {WalletComponent} from './wallet/wallet.component'
+import {HomeComponent} from './home/home.component'
+import {LayoutComponent} from './ui/layout.component'
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    // If this path is the 'full' match...
+    pathMatch: 'full',
+    // ...redirect to this route.
+    redirectTo:'home',
+  },
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      { path: 'home', component: HomeComponent },
+      { path: 'wallet', component: WalletComponent },
+
+    ],
+  }
+];
